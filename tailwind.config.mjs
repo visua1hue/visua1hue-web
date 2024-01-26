cf1d040c75eb048c470fccc1899c031ec8fc6d6b
visua1hue <michael.huebel@icloud.com>
2024-01-26 12:58:22 +0100

    Added Navbar and adjusted Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['Inter', 'sans-serif'],
+			},
+			colors: {
+				'black-shade': '#09090a',
+				'grey-shade': '#19191c',
+			}
+		},
 	},
 	plugins: [],
 }
