2c11a3d15f2a2ca1d4407c01dd021ee785485a8a
MikĒ <54352478+visua1hue@users.noreply.github.com>
2025-10-12 15:06:07 +0200

    Added CSS files, updated configs and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,49 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  // Site Metadata
+  site: 'https://visua1hue.dev',
+
+  // Build & Output Configuration
   output: 'server',
   adapter: cloudflare(),
+
+  // Integrations
+  integrations: [
+    icon({
+      iconDir: 'src/assets/icons',
+    }),
+  ],
+
+  // Image Optimization
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    }
+  },
+
+  // Prefetch Performance Tuning
+  prefetch: {
+    defaultStrategy: 'hover',
+  },
+
+  // Vite Configuration
   vite: {
     ssr: {
       external: ['node:buffer']
-    }
-  }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            motion: ['motion'],
+          },
+        },
+      },
+    },
+  },
 });
